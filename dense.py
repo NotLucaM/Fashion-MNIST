c2f644a5b3edf31dee8e9c3611b10eb16abb1afa
luca <luca1230@gmail.com>
2020-06-12 17:55:42 -0700

    fixed bug and wrote comment

--- dense.py
+++ dense.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 
 from tensorflow import keras
 
+# A simple (my first) implementation of an AI for the fashion_mnist dataset; achieves 0.3493 loss
+
 dataset = keras.datasets.fashion_mnist
 
 (training_images, training_labels), (test_images, test_labels) = dataset.load_data()
@@ -10,9 +12,9 @@ dataset = keras.datasets.fashion_mnist
 training_images = training_images / 255
 test_images = test_images / 255
 
-model = keras.Sequential(keras.layers.Flatten(input_shape=(28, 28)),
+model = keras.Sequential([keras.layers.Flatten(input_shape=(28, 28)),
                          keras.layers.Dense(128, activation=tf.nn.relu),
-                         keras.layers.Dense(10, activation=tf.nn.softmax))
+                         keras.layers.Dense(10, activation=tf.nn.softmax)])
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
 model.fit(training_images, training_labels, epochs=5)
